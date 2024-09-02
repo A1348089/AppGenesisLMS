ea7c627687e53b325f556d0242bd7b84fc308baf
Aravind <116650457+A1348089@users.noreply.github.com>
2024-09-02 13:13:35 +0530

    first commit

--- questions/api/serializers.py
+++ questions/api/serializers.py
@@ -5,4 +5,4 @@ from questions.models import Questions
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Questions
-        fields = ['id', 'question', 'answer']
+        fields = '__all__'
